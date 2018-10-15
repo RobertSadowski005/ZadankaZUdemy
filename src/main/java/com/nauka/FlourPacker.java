ebbdc1bbff52872dd6703f0d540450d1414f6c5d
Robert Sadowski <robertsadowski005@gmail.com>
2018-10-15 14:29:09 +0200

    Exercise from Udemy.com.

--- src/main/java/com/nauka/FlourPacker.java
+++ src/main/java/com/nauka/FlourPacker.java
@@ -2,10 +2,38 @@ package com.nauka;
 
 public class FlourPacker {
     public static void main(String[] args) {
-        canPack(1, 0 , 5);
+        System.out.println(canPack(0, 5, 6));
 
     }
-    public static void canPack(int bigCount, int smallCount, int goal){
 
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        int counterBigCount = 0, counterSmallCount = 0;
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        } else {
+            for (int i = 0; i < bigCount; i++) {
+                if (goal == 0) return true;
+                else {
+                    goal -= 5;
+                    if (goal < 0) {
+                        goal += 5;
+                    }
+                }
+                System.out.println("After Big: " + goal);
+            }
+            for (int i = 0; i < smallCount; i++) {
+                if (goal == 0) {
+                    return true;
+                } else {
+                    goal -= 1;
+                    if (goal < 0) {
+                        goal += 1;
+                    }
+                }
+            }System.out.println("After small: " + goal);
+            System.out.println("What is left from goal: "+goal);
+            if (goal >0)return false;
+            return true;
+        }
     }
 }
