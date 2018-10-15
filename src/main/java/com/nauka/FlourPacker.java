99ac85c87549b965a85a404a827d3fde2ba6d643
Robert Sadowski <robertsadowski005@gmail.com>
2018-10-15 11:45:38 +0200

    Beginning of the CanPacking exercise.

---
+++ src/main/java/com/nauka/FlourPacker.java
@@ -0,0 +1,11 @@
+package com.nauka;
+
+public class FlourPacker {
+    public static void main(String[] args) {
+        canPack(1, 0 , 5);
+
+    }
+    public static void canPack(int bigCount, int smallCount, int goal){
+
+    }
+}
