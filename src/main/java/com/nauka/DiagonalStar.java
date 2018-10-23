bbd94220cfcf7c8ea3e07289fd20e91bf08e4efa
Robert Sadowski <robertsadowski005@gmail.com>
2018-10-23 17:58:16 +0200

    Start to a new exercise. Program writes stars.

--- src/main/java/com/nauka/DiagonalStar.java
+++ src/main/java/com/nauka/DiagonalStar.java
@@ -5,11 +5,31 @@ public class DiagonalStar {
         printSquareStar(5);
     }
 
-    private static void printSquareStar(int number) {
-        for (int i = 0; i < number; i++) {
-            System.out.println();
-            for (int j = 0; j < number; j++) {
-                System.out.print("*");
+    public static void printSquareStar(int number) {
+        if (number< 5)
+            System.out.println("Invalid Value");
+        else {
+            for (int i = 0; i < number; i++) {
+                System.out.println();
+                for (int j = 0; j < number; j++) {
+
+                    if (i == 0)
+                        System.out.print("*");
+                    else if (i == number - 1)
+                        System.out.print("*");
+                    else if (j == 0)
+                        System.out.print("*");
+                    else if (j == number - 1)
+                        System.out.print("*");
+                    else if (i == j)
+                        System.out.print("*");
+                    else if (i + j == number - 1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+
+
+                }
             }
         }
     }
